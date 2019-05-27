4ef85df5d85c932302849eb69f25ced001f386f7
estherkoko <estherkolade@gmail.com>
2019-05-27 19:27:55 -0400

    NewTicket.jsx - added submission form and submit button

--- src/components/Ticket/NewTicket.jsx
+++ src/components/Ticket/NewTicket.jsx
@@ -1,23 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TicketPerson from '../../assets/images/ticket.svg'
 
 class NewTicket extends Component {
     render() {
         return (
             <div className="container">
-               <div className="row">
-                   <div className="col-md-6">
+                <div className="row mx-5">
+                    <div className="col-md-6 mt-4">
                         <p className="mt-5 mb-0 text-muted">Hi Damola,</p>
                         <h1 className="font-weight-bold">Submit a ticket</h1>
                         <div>
-                        <p className="text-muted w-75">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet massa ac egestas sollicitudin. Curabitur hendrerit vestibulum justo at tristique. Nulla lacinia tellus nec neque accumsan auctor.</p>
-                        <img src={TicketPerson} alt="ticket" className="ticket__img"/>
+                            <p className="text-muted w-75">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet massa ac egestas sollicitudin. Curabitur hendrerit vestibulum justo at tristique. Nulla lacinia tellus nec neque accumsan auctor.</p>
+                            <img src={TicketPerson} alt="ticket" className="ticket__img" />
                         </div>
-                   </div>
-                   <div className="col-md-6">
-                
-                   </div>
-               </div>
+                    </div>
+                    <div className="col-md-6 mt-5">
+                        <form className="mt-4 w-75">
+                            <div className="form-group">
+                                <label className="text-muted font-weight-normal">Support Category</label>
+                                <input type="text" className="form-control bg-light" id="support-category" />
+                            </div>
+                            <div className="form-group">
+                                <label className="text-muted">Subject</label>
+                                <input type="text" className="form-control bg-light" id="subject" />
+                            </div>
+                            <div className="form-group">
+                                <label className="text-muted">Description</label>
+                                <textarea className="form-control bg-light" id="description" rows="5"></textarea>
+                            </div>
+                            <div className="form-group">
+                                <button type="button" className="btn btn-danger rounded-0 w-100 text-left">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
