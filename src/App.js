23cfa67ad0517a6bcc84cfdcafdb594694f351e1
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-05-27 14:34:25 +0100

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.scss';
-import Home from "./components/home/home";
+import Home from './pages/Home';
 import Header from "./components/Header";
 import NewTicket from "./components/Ticket/NewTicket";
 
