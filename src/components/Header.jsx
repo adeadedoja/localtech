515be4bb2a616db940e3f07d9157abbbee3771ba
adeadedoja <damdey@yahoo.com>
2019-05-28 07:58:48 +0200

    master - turns out, we need the collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,19 @@ import { FiUser, FiPlus, FiUserPlus } from 'react-icons/fi';
 
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
     render() {
         return (
             <div>
