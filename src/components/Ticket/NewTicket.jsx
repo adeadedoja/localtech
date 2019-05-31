cfee72d8f67a9c990c7e6bb96688da37e8c28b1b
estherkoko <estherkolade@gmail.com>
2019-05-31 18:44:37 -0400

    fixed spacing between div elements, made other changes per PR

--- src/components/Ticket/NewTicket.jsx
+++ src/components/Ticket/NewTicket.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TicketPerson from '../../assets/images/ticket.svg';
+import TicketImage from '../../assets/images/ticket.svg';
 import NewTicketForm from '../Ticket/NewTicketForm';
 
 
@@ -7,16 +7,16 @@ class NewTicket extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row col-md-10 offset-md-1">
-                    <div className="col-md-6 mt-4">
-                        <p className="mt-5 mb-0 text-muted">Hi Damola,</p>
+                <div className="row my-5 justify-content-around">
+                    <div className="col-md-4 my-4">
+                        <p className="mb-0 text-muted">Hi Damola,</p>
                         <h1 className="font-weight-bold">Submit a ticket</h1>
                         <div>
                             <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet massa ac egestas sollicitudin. Curabitur hendrerit vestibulum justo at tristique. Nulla lacinia tellus nec neque accumsan auctor.</p>
-                            <img src={TicketPerson} alt="ticket" className="ticket__img" />
+                            <img src={TicketImage} alt="ticket" className="ticket__img" />
                         </div>
                     </div>
-                    <div className="col-md-6 mt-5">
+                    <div className="col-md-4">
                         <NewTicketForm />
                     </div>
                 </div>
--- src/components/Ticket/NewTicketForm.jsx
+++ src/components/Ticket/NewTicketForm.jsx
@@ -5,9 +5,9 @@ import { FiArrowRight } from 'react-icons/fi';
 class NewTicketForm extends Component {
     render() {
         return (
-            <form className="mt-4">
+            <form className="my-4">
                 <div className="form-group">
-                    <label className="text-muted font-weight-normal">Support Category</label>
+                    <label className="text-muted">Support Category</label>
                     <input type="text" className="form-control bg-light" id="support-category" />
                 </div>
                 <div className="form-group">
@@ -19,7 +19,10 @@ class NewTicketForm extends Component {
                     <textarea className="form-control bg-light" id="description" rows="5"></textarea>
                 </div>
                 <div className="form-group">
-                    <button type="button" className="btn btn-danger rounded-0 w-100 p-2 px-3 text-left">Submit <span className="float-right"><FiArrowRight /></span></button>
+                    <button type="button" className="btn btn-danger rounded-0 w-100 p-2 d-flex justify-content-between">
+                        <span>Submit</span> 
+                        <span><FiArrowRight /></span>
+                    </button>
                 </div>
             </form>
         );
